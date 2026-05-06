4401b2fa396a331a8332a9ee3c3bf64e50c94b38
Trithereon <eliottrassa@gmail.com>
2026-05-06 14:50:29 -0400

    Add test

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,4 +1,7 @@
 import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+import App from "../App";
 
 describe("something truthy and falsy", () => {
   it("true to be true", () => {
@@ -7,5 +10,15 @@ describe("something truthy and falsy", () => {
 
   it("false to be false", () => {
     expect(false).toBe(false);
+  });
+});
+
+describe("App", () => {
+  it("renders headline", () => {
+    render(<App title="React" />);
+
+    screen.debug();
+
+    // Check if App component renders headline.
   });
 });
